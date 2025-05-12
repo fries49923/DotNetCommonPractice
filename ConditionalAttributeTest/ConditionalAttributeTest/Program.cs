ee969356d9a51da62fe11f3b55ce1da5caf877f8
ポテト <fries49923@gmail.com>
2025-05-13 00:26:01 +0800

    Add Sln ConditionalAttributeTest
    1. Add Sln ConditionalAttributeTest

---
+++ ConditionalAttributeTest/ConditionalAttributeTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace ConditionalAttributeTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("程式開始");
+
+#if DEBUG
+            Console.WriteLine("這段在 DEBUG 模式下才會被編譯進來");
+#else
+            Console.WriteLine("這段在 非 DEBUG 模式 (如 Release) 下才會被編譯進來");
+#endif
+
+            // 一定會執行
+            LogMessage();
+
+            // 只有在 DEBUG 模式下會執行
+            LogMessageDebug();
+
+            Console.WriteLine("程式結束");
+
+            Console.ReadKey();
+        }
+
+        static void LogMessage()
+        {
+            Console.WriteLine("LogMessage() 不論模式都會執行");
+        }
+
+        [Conditional("DEBUG")]
+        static void LogMessageDebug()
+        {
+            Console.WriteLine("LogMessageDebug() 只在 DEBUG 模式下會執行");
+        }
+    }
+}
---
+++ ConditionalAttributeTest/README.md
@@ -0,0 +1,3 @@
+#條件式編譯 #ConditionalAttribute
+
+測試如何使用 [Conditional("DEBUG")] 屬性，在 Debug 模式下條件性執行特定方法，並與 #if DEBUG 的條件式編譯進行比較
